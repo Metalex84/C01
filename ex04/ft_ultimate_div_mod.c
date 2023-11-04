c2ad6ded85964bec903d30cc26a53df992679c9d
Alejandro Gonzalez Venegas <alex@MacBook-Air-de-Alejandro.local>
2023-11-04 14:15:19 +0100

    ft_ultimate_div_mod with main

---
+++ ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejango <alejango@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/14 11:27:48 by alejango          #+#    #+#             */
+/*   Updated: 2023/08/14 11:28:46 by alejango         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	division;
+	int	modulo;
+
+	division = *a / *b;
+	modulo = *a % *b;
+	*a = division;
+	*b = modulo;
+}
